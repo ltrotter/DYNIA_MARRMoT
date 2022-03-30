b0c2734c12e2168848ae070dafadd12be30d45d1
ltrotter <lucatrotter@gmail.com>
2022-03-30 12:00:18 +0200

    Add support for missing Qobs

--- dynia.m
+++ dynia.m
@@ -10,14 +10,20 @@ Qsim = run_with_par_sample(model, theta_sample);
 % calculate performance over a moving window of width window
 perf_over_time = calc_of_moving_window(Qsim, Qobs, window, of_name);
 
+% from this point on, timesteps with missing Qsim (where we couldn't
+% calculate the objective function) can just be ignored, as long as we keep
+% track of their position to re-add them in the end as missing rows
+idx_non_missing = find(~isnan(perf_over_time(:,1)));
+perf_over_time_non_missing = perf_over_time(idx_non_missing,:);
+
 % keep top 10% of performances, which_top tells you the idx of the
 % parameters used
-[perf_top_10, which_top_10] = maxk(perf_over_time, round(n/10),2);
+[perf_top_10, which_top_10] = maxk(perf_over_time_non_missing, round(n/10),2);
 % make sure all performances are >=0 by adding the minimum performance
 perf_top_10_pos = perf_top_10 - min(min(perf_top_10,[],'all'),0);
 
 % create empty container for best thetas for each timestep
-theta_top_10 = zeros(numel(Qobs), round(n/10), model.numParams);
+theta_top_10 = zeros(numel(idx_non_missing), round(n/10), model.numParams);
 % populate with the best sets of theta for each timestep
 for t=1:size(theta_top_10,1)
     theta_top_10(t,:,:) = theta_sample(:,which_top_10(t,:))';
@@ -27,7 +33,14 @@ end
 [cd_top_10, theta_top_10_sorted] = calc_cd_multidim(theta_top_10, perf_top_10_pos);
 
 % calculate information content for each parameter at each timestep
-info_content = calc_info_content(cd_top_10, theta_top_10_sorted);
+info_content_non_missing = calc_info_content(cd_top_10, theta_top_10_sorted);
 
 % calculate gradients of the (binned) cumulative distribution
-cd_gradient = calc_cd_gradient(cd_top_10, theta_top_10_sorted);
+cd_gradient_non_missing = calc_cd_gradient(cd_top_10, theta_top_10_sorted);
+
+%re-add in the timesteps with missing data
+info_content = NaN(numel(Qobs), model.numParams);
+info_content(idx_non_missing,:) = info_content_non_missing;
+
+cd_gradient = NaN(numel(Qobs), size(cd_gradient_non_missing,2), model.numParams);
+cd_gradient(idx_non_missing,:,:) = cd_gradient_non_missing;
