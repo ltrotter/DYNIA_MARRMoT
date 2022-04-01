4ddbb63bb2ad786026fab1387821efd4a0d34bd1
ltrotter <lucatrotter@gmail.com>
2022-04-01 15:07:43 +0200

    Add control for precision

--- dynia.m
+++ dynia.m
@@ -1,6 +1,7 @@
 function [cd_gradient,...
           cd_gradient_breaks,...
-          info_content] = ...
+          info_content,...
+          OF_idx] = ...
                           dynia(model, Qobs, options)
 
 defaultopt = struct('repeats', 2e5, ...
@@ -8,7 +9,8 @@ defaultopt = struct('repeats', 2e5, ...
                     'window_size', 31, ...
                     'window_step', 1,...
                     'file_prefix', 'DYNIA', ...
-                    'chunk_size', 1000);%, 'Display', 'off');
+                    'chunk_size', 1000,...
+                    'precision', 4);%, 'Display', 'off');
 
 if nargin < 3 || isempty(options); options = struct(); end
 
@@ -19,6 +21,7 @@ window = optimget(options, 'window_size', defaultopt, 'fast');
 step = optimget(options, 'window_step', defaultopt, 'fast');
 file_prefix = optimget(options, 'file_prefix', defaultopt, 'fast');
 c_size = optimget(options, 'chunk_size', defaultopt, 'fast');
+precision = optimget(options, 'precision', defaultopt, 'fast');
 
 file_theta = [file_prefix, '_theta_samples.csv'];
 file_Qsim  = [file_prefix, '_Q_sim.csv'];
@@ -31,9 +34,9 @@ file_perf  = [file_prefix, '_OF_value.csv'];
 % if this is the first time you run this, write the options used at the top
 % of the file, so that they can be checked afterwards
 header = ['OF = ', of_name, '; window size = ', int2str(window), '; window step = ', int2str(step)];
+OF_idx = (floor(window/2)+1):step:(numel(Qobs)-floor(window/2)-1);
 if any(~isfile(file_perf))
     n_to_do = n;
-    OF_idx = (floor(window/2)+1):step:(numel(Qobs)-floor(window/2)-1);
     writematrix(header, file_perf);
     writematrix(OF_idx, file_perf, "WriteMode", "append");
 else
@@ -61,8 +64,8 @@ while chunks > 0
     % write both to file (appending to make sure you don't lose the values 
     % from the previous chunks), so that it can be retrieved afterwards
     writematrix(theta_sample_chunk', file_theta, "WriteMode", "append");
-    writematrix(Qsim_chunk', file_Qsim, "WriteMode", "append");
-    writematrix(perf_over_time_chunk', file_perf, "WriteMode", "append");
+    writematrix(round(Qsim_chunk, precision)', file_Qsim, "WriteMode", "append");
+    writematrix(round(perf_over_time_chunk, precision)', file_perf, "WriteMode", "append");
 
     chunks = chunks - 1;
 end
@@ -70,8 +73,7 @@ end
 % after all simulations are finished; open the three final files and
 % continue
 theta_sample = readmatrix(file_theta)';
-Qsim = readmatrix(file_Qsim)';
-perf_over_time = readmatrix(file_perf_over_time)';
+perf_over_time = readmatrix(file_perf, 'NumHeaderLines',2)';
 
 % from this point on, timesteps with missing Qsim (where we couldn't
 % calculate the objective function) can just be ignored, as long as we keep
@@ -102,8 +104,8 @@ info_content_non_missing = calc_info_content(cd_top_10, theta_top_10_sorted);
 [cd_gradient_breaks, cd_gradient_non_missing] = calc_cd_gradient(cd_top_10, theta_top_10_sorted);
 
 %re-add in the timesteps with missing data
-info_content = NaN(numel(Qobs), model.numParams);
+info_content = NaN(numel(OF_idx), model.numParams);
 info_content(idx_non_missing,:) = info_content_non_missing;
 
-cd_gradient = NaN(numel(Qobs), size(cd_gradient_non_missing,2), model.numParams);
+cd_gradient = NaN(numel(OF_idx), size(cd_gradient_non_missing,2), model.numParams);
 cd_gradient(idx_non_missing,:,:) = cd_gradient_non_missing;
