62b48f22371ed3704f409220816ca8720bfaf7a0
ltrotter <lucatrotter@gmail.com>
2022-05-04 12:22:20 +0200

    Condense all functions in one file

--- lhs_sample_par.m
+++
@@ -1,5 +0,0 @@
-function sample = lhs_sample_par(model, n)
-
-ranges = model.parRanges;
-lhs = lhsdesign(n,size(ranges, 1));
-sample = lhs'.*diff(ranges,1,2) + ranges(:,1);
--- run_dynia_simulations.m
+++ run_dynia_simulations.m
@@ -37,12 +37,12 @@ end
 end
 
 function [] = helper_sim_function(file_log, simdata, model, Qobs, o)
-    
+
     n_done = simdata.n_done;
     theta_sample = simdata.theta_sample;
     OF_idx = simdata.OF_idx;
     pc_done = simdata.pc_done;
-    
+
     if n_done == 0
         % create a csv file for each timestep
         % create the header, which is common for all files
@@ -57,39 +57,37 @@ function [] = helper_sim_function(file_log, simdata, model, Qobs, o)
             fprintf(fileID, header);
             fclose(fileID);
         end
-        %simdata.isfirst = ones(size(OF_idx)); % this is used to add commas between entries in the json files
     end
-    %isfirst = simdata.isfirst;
-    
+
     % loop through each set of thetas
     while n_done < o.n
         this_theta = theta_sample(:,n_done + 1);
-    
+
         % run the modoel with this parameter set
         model.theta = this_theta; model.run();
-    
+
         % get the streamflow
         Qsim = model.get_streamflow();
-    
+
         % calculate performance over time
         perf_over_time = calc_of_moving_window(Qsim, Qobs, o.window, o.step, o.of_name, o.precision_Q+1, o.of_args{:});
-    
+
         % check the timesteps with performance above the threshold
         behavioural_steps = perf_over_time > o.perf_thr;
         OF_idx_behavioural = OF_idx(behavioural_steps);
-    
+
         % extract performance, fluxes and stores at those timesteps
         OF_vals = perf_over_time(behavioural_steps);
         fluxes = calc_avg_at_timesteps(model.fluxes, OF_idx_behavioural, o.window);
         stores = calc_avg_at_timesteps(model.stores, OF_idx_behavioural, o.window);
-    
+
         % for each of those steps
         for t = 1:numel(OF_idx_behavioural)
             % create a cell array containing the useful data
             OF_here = round(OF_vals(t), o.precision_OF);
             fluxes_here = round(fluxes(t,:), o.precision_Q);
             stores_here = round(stores(t,:), o.precision_Q);
-    
+
             % create the name and text to save as json
             this_OF_idx = OF_idx_behavioural(t);
             file_name = [o.file_prefix '_' num2str(this_OF_idx) '.csv'];
@@ -102,11 +100,11 @@ function [] = helper_sim_function(file_log, simdata, model, Qobs, o)
             fprintf(fileID, csv_txt);
             fclose(fileID);
         end
-    
+
         % increase n_done and add it to the log file
         n_done = n_done + 1;
         simdata.n_done = n_done;
-    
+
         % display progress at each full percentage points
         if(o.display)
             pc_done_now = n_done/o.n * 100;
@@ -120,4 +118,89 @@ function [] = helper_sim_function(file_log, simdata, model, Qobs, o)
         simdata.pc_done = pc_done;
         save(file_log, "simdata", "-append");
     end
+end
+
+function sample = unif_sample_par(model, n)
+
+    ranges = model.parRanges;
+    rd = rand(size(ranges, 1),n);
+    sample = rd.*diff(ranges,1,2) + ranges(:,1);
+
+end
+
+function sample = lhs_sample_par(model, n)
+
+    ranges = model.parRanges;
+    lhs = lhsdesign(n,size(ranges, 1));
+    sample = lhs'.*diff(ranges,1,2) + ranges(:,1);
+
+end
+
+function opts_out = get_dynia_options(opts_in)
+
+    defaultopt = struct('repeats', 2e5, ...
+                        'of_name', 'of_KGE', ...
+                        'window_size', 31, ...
+                        'window_step', 1,...
+                        'file_prefix', 'DYNIA', ...
+                        'chunk_size', 1000,...
+                        'precision_Q', 4, ...
+                        'precision_OF', 4,...
+                        'perf_thr', 0,...
+                        'display', 1,...
+                        'overwrite', 0);
+    defaultopt.of_args = cell(0);
+
+    % get options
+    opts_out.n = optimget(opts_in, 'repeats', defaultopt, 'fast');
+    opts_out.of_name = optimget(opts_in, 'of_name', defaultopt, 'fast');
+    opts_out.window = optimget(opts_in, 'window_size', defaultopt, 'fast');
+    opts_out.step = optimget(opts_in, 'window_step', defaultopt, 'fast');
+    opts_out.file_prefix = optimget(opts_in, 'file_prefix', defaultopt, 'fast');
+    opts_out.precision_Q = optimget(opts_in, 'precision_Q', defaultopt, 'fast');
+    opts_out.precision_OF = optimget(opts_in, 'precision_OF', defaultopt, 'fast');
+    opts_out.of_args = optimget(opts_in, 'of_args', defaultopt, 'fast');
+    opts_out.perf_thr = optimget(opts_in, 'perf_thr', defaultopt, 'fast');
+    opts_out.display = optimget(opts_in, 'display', defaultopt, 'fast');
+    opts_out.overwrite = optimget(opts_in, 'overwrite', defaultopt, 'fast');
+
+end
+
+function [of_over_time_non_missing, idx_non_missing] = calc_of_moving_window(Qsim,Qobs,window,step,of_name,precision,varargin)
+
+    if isempty(precision); precision = 3; end
+    [T,n] = size(Qsim);
+
+    half_window = floor(window/2);
+    idx = (half_window+1):step:(T-half_window-1);
+    of_over_time = NaN(numel(idx),n);
+    for i = 1:numel(idx)
+        rand_perm = rand(2*half_window+1,1)*10^-precision;
+        this_idx = idx(i);
+        Qobs_in_window = Qobs(this_idx-half_window:this_idx+half_window) + rand_perm;
+        if any(Qobs_in_window < 0) || any(isnan(Qobs_in_window)); continue; end
+        Qsim_in_window = Qsim(this_idx-half_window:this_idx+half_window,:) + rand_perm;
+        for p = 1:n
+            of_over_time(i,p) = feval(of_name,Qobs_in_window,Qsim_in_window(:,p),varargin{:});
+        end
+    end
+
+    missing_timesteps = isnan(of_over_time(:,1));
+    of_over_time_non_missing = of_over_time(~missing_timesteps,:);
+    idx_non_missing = idx(~missing_timesteps);
+
+end
+
+function [average_at_timesteps] = calc_avg_at_timesteps(data, timesteps, window)
+
+    half_window = floor(window/2);
+    T = numel(timesteps);
+
+    average_at_timesteps = NaN(T,size(data,2));
+
+    for i=1:T
+        t = timesteps(i);
+        average_at_timesteps(i,:) = mean(data(t-half_window:t+half_window,:),1);
+    end
+
 end
