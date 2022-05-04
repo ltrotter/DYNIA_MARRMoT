6c987be7f29298db7e3af75f90b1c74510cb2daf
ltrotter <lucatrotter@gmail.com>
2022-05-04 12:21:57 +0200

    Remove all analysis scripts

--- run_with_par_sample.m
+++
@@ -1,9 +0,0 @@
-function Qout = run_with_par_sample(m, theta_sample)
-
-n = size(theta_sample,2);
-Qout = zeros(size(m.input_climate,1), n);
-
-for i = 1:n
-    m.theta = theta_sample(:,i);
-    Qout(:,i) = m.get_streamflow();
-end
