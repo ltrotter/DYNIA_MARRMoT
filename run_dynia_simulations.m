5a507d67c4692fc0abb98427445e0bb8deee1e56
ltrotter <lucatrotter@gmail.com>
2022-05-27 12:11:03 +0200

    parallel evaluation

--- run_dynia_simulations.m
+++ run_dynia_simulations.m
@@ -60,88 +60,190 @@ function [] = helper_sim_function(file_log, simdata, model, Qobs, o)
         end
     end
 
-    % before we start the loop, prepare the output cell and start a timer
-    this_chunk_output = cell(numel(OF_idx),1);
-    chunk_time = tic;
-
-    % loop through each set of thetas
-    while n_done < o.n
-        this_theta = theta_sample(:,n_done + 1);
-
-        % run the modoel with this parameter set
-        model.theta = this_theta; model.run();
-
-        % get the streamflow
-        Qsim = model.get_streamflow();
-
-        % calculate performance over time
-        perf_over_time = calc_of_moving_window(Qsim, Qobs, o.window, o.step, o.of_name, o.precision_Q+1, o.of_args{:});
-
-        % check the timesteps with performance above the threshold
-        behavioural_steps = perf_over_time > o.perf_thr;
-        OF_idx_behavioural = OF_idx(behavioural_steps);
-        idx_behavioural = find(behavioural_steps);
-
-        % extract performance, fluxes and stores at those timesteps
-        OF_vals = perf_over_time(behavioural_steps);
-        fluxes = calc_avg_at_timesteps(model.fluxes, OF_idx_behavioural, o.window);
-        stores = calc_avg_at_timesteps(model.stores, OF_idx_behavioural, o.window);
-
-        % for each of those steps
-        for i = 1:numel(idx_behavioural)
-            t = idx_behavioural(i);
-
-            % get all the useful data
-            OF_here = round(OF_vals(i), o.precision_OF);
-            fluxes_here = round(fluxes(i,:), o.precision_Q);
-            stores_here = round(stores(i,:), o.precision_Q);
-
-            % create the one line string that will need to be written to
-            % file
-            csv_txt = [num2str(this_theta', '%.9g,'), num2str(OF_here, '%g,'),...
-                       num2str(fluxes_here, '%g,'),  num2str(stores_here, '%g,')];
-            csv_txt(end) = []; csv_txt = [csv_txt, '\n'];
-
-            % append it to what's already existing
-            this_chunk_output{t} = [this_chunk_output{t}, csv_txt];
-        end
-
-        % increase n_done
-        n_done = n_done + 1;
-
-        % each chunk_size simulations, save the results so far
-        if rem(n_done, o.chunk_size) == 0
-            for c=1:numel(this_chunk_output)
-                if isempty(this_chunk_output{c}); continue; end
-                % create the file name
-                file_name = [o.file_prefix '_' num2str(OF_idx(c)) '.csv'];
-                
-                % save the relevant output
-                fileID = fopen(file_name,'a');
-                fprintf(fileID, this_chunk_output{c});
-                fclose(fileID);
+    if o.parallelEval == 1 %parallel evaluation
+        poolobj = gcp('nocreate');
+        if isempty(poolobj); poolobj = parpool; end
+        workers = poolobj.NumWorkers;
+        while n_done < o.n
+            n_to_do = o.n - n_done;
+            if n_to_do < workers * o.chunk_size
+                o.chunk_size = ceil(n_to_do/workers);
             end
-
-            % empty up this_chunk_output
-            this_chunk_output = cell(numel(OF_idx),1);
+            spmd_start = tic;
+            spmd
+%                 this_worker_data = load([o.file_prefix 'workers_data.mat']);
+%                 OF_idx = this_worker_data.OF_idx;
+%                 model = this_worker_data.model;
+%                 numel_OF_idx = this_worker_data.numel_OF_idx;
+%                 Qobs = this_worker_data.Qobs;
+%                 o = this_worker_data.o;
+                this_start_i = n_done + 1 + (labindex-1)*o.chunk_size;
+                this_end_i = min(this_start_i + o.chunk_size - 1, size(theta_sample,2));
+                this_theta_sample = theta_sample(:,this_start_i:this_end_i);
+                this_output = cell(numel(OF_idx),1);
+                for j=1:size(this_theta_sample,2)
+                    this_theta = this_theta_sample(:,j);
+        
+                    % run the model with this parameter set
+                    model.theta = this_theta; model.run();
+                
+                    % get the streamflow
+                    Qsim = model.get_streamflow();
+                     
+                    % calculate performance over time
+                    perf_over_time = calc_of_moving_window(Qsim, Qobs, o.window, o.step, o.of_name, o.precision_Q+1, o.of_args{:});
+            
+                    % check the timesteps with performance above the threshold
+                    behavioural_steps = perf_over_time > o.perf_thr;
+                    OF_idx_behavioural = OF_idx(behavioural_steps);
+                    idx_behavioural = find(behavioural_steps);
+            
+                    % extract performance, fluxes and stores at those timesteps
+                    OF_vals = perf_over_time(behavioural_steps);
+                    fluxes = calc_avg_at_timesteps(model.fluxes, OF_idx_behavioural, o.window);
+                    stores = calc_avg_at_timesteps(model.stores, OF_idx_behavioural, o.window);
+        
+                    % for each of those steps
+                    for i = 1:numel(idx_behavioural)
+                        t = idx_behavioural(i);
+            
+                        % get all the useful data
+                        OF_here = round(OF_vals(i), o.precision_OF);
+                        fluxes_here = round(fluxes(i,:), o.precision_Q);
+                        stores_here = round(stores(i,:), o.precision_Q);
+            
+                        % create the one line string that will need to be written to
+                        % file
+                        csv_txt = [num2str(this_theta', '%.9g,'), num2str(OF_here, '%g,'),...
+                                   num2str(fluxes_here, '%g,'),  num2str(stores_here, '%g,')];
+                        csv_txt(end) = []; csv_txt = [csv_txt, '\n'];
+            
+                        % append it to what's already existing
+                        this_output{t} = [this_output{t}, csv_txt];
+                    end
+                end
+                
+                % write outputs of all timesteps to file, after the end of the loop
+                for c=1:numel(this_output)
+                    if isempty(this_output{c}); continue; end
+                    % create the file name
+                    file_name = [o.file_prefix '_' num2str(OF_idx(c)) '.csv'];
+                    
+                    % save the relevant output
+                    fileID = fopen(file_name,'a');
+                    fprintf(fileID, this_output{c});
+                    fclose(fileID);
+                end
+                
+                done_these_chunks = j;
+            end
+            n_done = n_done + sum([done_these_chunks{:}]);
 
             % calculate time it took for this chunk
-            this_chunk_time = toc(chunk_time); chunk_time = tic;
-            total_time = total_time + this_chunk_time;
-
+            these_chunks_time = toc(spmd_start);
+            total_time = total_time + these_chunks_time;
+        
             % save n_done to the log file
             simdata.n_done = n_done;
-            simdata.chunk_time(end+1) = this_chunk_time;
+            simdata.chunk_time(end+1) = these_chunks_time;
             simdata.total_time = total_time;
             save(file_log, "simdata", "-append");
-
+        
             % print to screen if display is active
             if o.display
                 msg = ['Simulations done: ', num2str(n_done), '/', num2str(o.n),...
                             ' (', num2str(n_done/o.n*100,'%.2f'), '%) - ',...
                             'Elapsed time = ' num2str(total_time/60,'%.2f'), 'min (+',...
-                            num2str(this_chunk_time/60,'%.2f'),')'];
+                            num2str(these_chunks_time/60,'%.2f'),')'];
                 disp(msg);
+            end
+        end
+        delete(poolobj)
+
+    else % series evaluation
+        % before we start the loop, prepare the output cell and start a timer
+        this_chunk_output = cell(numel(OF_idx),1);
+        chunk_time = tic;
+    
+        % loop through each set of thetas
+        while n_done < o.n
+            this_theta = theta_sample(:,n_done + 1);
+    
+            % run the model with this parameter set
+            model.theta = this_theta; model.run();
+    
+            % get the streamflow
+            Qsim = model.get_streamflow();
+    
+            % calculate performance over time
+            perf_over_time = calc_of_moving_window(Qsim, Qobs, o.window, o.step, o.of_name, o.precision_Q+1, o.of_args{:});
+    
+            % check the timesteps with performance above the threshold
+            behavioural_steps = perf_over_time > o.perf_thr;
+            OF_idx_behavioural = OF_idx(behavioural_steps);
+            idx_behavioural = find(behavioural_steps);
+    
+            % extract performance, fluxes and stores at those timesteps
+            OF_vals = perf_over_time(behavioural_steps);
+            fluxes = calc_avg_at_timesteps(model.fluxes, OF_idx_behavioural, o.window);
+            stores = calc_avg_at_timesteps(model.stores, OF_idx_behavioural, o.window);
+    
+            % for each of those steps
+            for i = 1:numel(idx_behavioural)
+                t = idx_behavioural(i);
+    
+                % get all the useful data
+                OF_here = round(OF_vals(i), o.precision_OF);
+                fluxes_here = round(fluxes(i,:), o.precision_Q);
+                stores_here = round(stores(i,:), o.precision_Q);
+    
+                % create the one line string that will need to be written to
+                % file
+                csv_txt = [num2str(this_theta', '%.9g,'), num2str(OF_here, '%g,'),...
+                           num2str(fluxes_here, '%g,'),  num2str(stores_here, '%g,')];
+                csv_txt(end) = []; csv_txt = [csv_txt, '\n'];
+    
+                % append it to what's already existing
+                this_chunk_output{t} = [this_chunk_output{t}, csv_txt];
+            end
+    
+            % increase n_done
+            n_done = n_done + 1;
+    
+            % each chunk_size simulations, save the results so far
+            if rem(n_done, o.chunk_size) == 0
+                for c=1:numel(this_chunk_output)
+                    if isempty(this_chunk_output{c}); continue; end
+                    % create the file name
+                    file_name = [o.file_prefix '_' num2str(OF_idx(c)) '.csv'];
+                    
+                    % save the relevant output
+                    fileID = fopen(file_name,'a');
+                    fprintf(fileID, this_chunk_output{c});
+                    fclose(fileID);
+                end
+    
+                % empty up this_chunk_output
+                this_chunk_output = cell(numel(OF_idx),1);
+    
+                % calculate time it took for this chunk
+                this_chunk_time = toc(chunk_time); chunk_time = tic;
+                total_time = total_time + this_chunk_time;
+    
+                % save n_done to the log file
+                simdata.n_done = n_done;
+                simdata.chunk_time(end+1) = this_chunk_time;
+                simdata.total_time = total_time;
+                save(file_log, "simdata", "-append");
+    
+                % print to screen if display is active
+                if o.display
+                    msg = ['Simulations done: ', num2str(n_done), '/', num2str(o.n),...
+                                ' (', num2str(n_done/o.n*100,'%.2f'), '%) - ',...
+                                'Elapsed time = ' num2str(total_time/60,'%.2f'), 'min (+',...
+                                num2str(this_chunk_time/60,'%.2f'),')'];
+                    disp(msg);
+                end
             end
         end
     end
@@ -181,7 +283,8 @@ function opts_out = get_dynia_options(opts_in)
                         'display', 1,...
                         'overwrite', 0,...
                         'theta', [],...
-                        'chunk_size', 1000);
+                        'chunk_size', 1000,...
+                        'parallelEval', 0);
     defaultopt.of_args = cell(0);
 
     % get options
@@ -198,6 +301,7 @@ function opts_out = get_dynia_options(opts_in)
     opts_out.overwrite = optimget(opts_in, 'overwrite', defaultopt, 'fast');
     opts_out.theta = optimget(opts_in, 'theta', defaultopt, 'fast');
     opts_out.chunk_size = optimget(opts_in, 'chunk_size', defaultopt, 'fast');
+    opts_out.parallelEval = optimget(opts_in, 'parallelEval', defaultopt, 'fast');
 end
 
 function [of_over_time_non_missing, idx_non_missing] = calc_of_moving_window(Qsim,Qobs,window,step,of_name,precision,varargin)
